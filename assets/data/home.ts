7220a5c937e0dc51b33f62e56fc546c56b3ea5a1
Loges <sivalog25@gmail.com>
2024-06-27 20:45:46 +0530

    initial commit

---
+++ assets/data/home.ts
@@ -0,0 +1,58 @@
+export const categories = [
+  {
+    text: 'Restaurants',
+    img: require('@/assets/data/c1.png'),
+  },
+  {
+    text: 'Grocery',
+    img: require('@/assets/data/c2.png'),
+  },
+  {
+    text: 'Offers',
+    img: require('@/assets/data/c3.png'),
+  },
+  {
+    text: 'Pickup',
+    img: require('@/assets/data/c4.png'),
+  },
+  {
+    text: 'HOP',
+    img: require('@/assets/data/c5.png'),
+  },
+  {
+    text: 'Pharmacy',
+    img: require('@/assets/data/c6.png'),
+  },
+];
+
+export const restaurants = [
+  {
+    name: 'La pina pizza ',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '0.7 miles away',
+    img: require('@/assets/data/c7.png'),
+    tags: ['Italian', 'Pizza', 'Pasta', 'Salads'],
+    duration: '35 - 45',
+  },
+  {
+    name: 'Dominos Pizza',
+    id: '2',
+    rating: '4.9 Excellent',
+    ratings: '(500+)',
+    distance: '1.7 miles away',
+    img: require('@/assets/data/r4.png'),
+    tags: ['Salads', 'Vegan', 'Healthy', 'British'],
+    duration: '15 - 30',
+  },
+  {
+    name: 'PizzaHut',
+    id: '3',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '3 miles away',
+    img: require('@/assets/data/r5.png'),
+    tags: ['Spanish', 'Salads', 'Tpas', 'Pasta'],
+    duration: '25 - 45',
+  },
+];
